b14b6355776d4d12ec11829c29581b142a3c1ae0
Martin Necasky <martinnec@gmail.com>
2021-01-12 13:54:32 +0100

    Opravy config.js

--- registr-práv-a-povinností/kategorie-soukromoprávních-uživatelů-údajů/2021-01-12/kategorie-soukromoprávních-uživatelů-údajů.config.js
+++ registr-práv-a-povinností/kategorie-soukromoprávních-uživatelů-údajů/2021-01-12/kategorie-soukromoprávních-uživatelů-údajů.config.js
@@ -2,15 +2,16 @@ var respecConfig = {
 	specStatus: "REC",
 	shortName: "rpp-kategorie-soukromoprávních-uživatelů-údajů",
 	showPreviousVersion: true,
-	previousVersion: "https://ofn.gov.cz/registr-práv-a-povinností/kategorie-soukromoprávních-uživatelů-údajů/2019-04-03/",
+	prevRecURI: "https://ofn.gov.cz/registr-práv-a-povinností/kategorie-soukromoprávních-uživatelů-údajů/2019-04-03/",
+	prevVersion: "https://ofn.gov.cz/registr-práv-a-povinností/kategorie-soukromoprávních-uživatelů-údajů/2019-04-03/",
 	thisVersion: "https://ofn.gov.cz/registr-práv-a-povinností/kategorie-soukromoprávních-uživatelů-údajů/2021-01-12/",
 	latestVersion: "https://ofn.gov.cz/registr-práv-a-povinností/kategorie-soukromoprávních-uživatelů-údajů/",
 	publishDate: "2021-01-12",
 	editors: [{
 	  name: "Martin Nečaský",
-	  url: "https://martin.necasky.solid.community/profile/card#me",
+	  orcid: "0000-0002-5186-7734",
 	  company:    "Ministerstvo vnitra",
-	  companyURL: "http://www.mvcr.cz"
+	  companyURL: "https://www.mvcr.cz"
 	}],
 	inlineCSS: "true",
 	github: "https://github.com/opendata-mvcr/otevrene-formalni-normy/",
@@ -19,6 +20,10 @@ var respecConfig = {
 		data: [{
 			value: "Portál otevřených dat",
 			href: "https://data.gov.cz"
+		},{
+			value: "Kontaktní e-mail",
+			href: "mailto:otevrenadata@mvcr.cz"
 		}]
 	}],
+	thanks: "Tento dokument vznikl v rámci projektu OPZ č. CZ.03.4.74/0.0/0.0/15_025/0013983."
 };
--- registr-práv-a-povinností/služby/2021-01-12/služby.config.js
+++ registr-práv-a-povinností/služby/2021-01-12/služby.config.js
@@ -1,15 +1,14 @@
 var respecConfig = {
 	specStatus: "REC",
 	shortName: "rpp-služby",
-	showPreviousVersion: true,
 	thisVersion: "https://ofn.gov.cz/registr-práv-a-povinností/služby/2021-01-12/",
 	latestVersion: "https://ofn.gov.cz/registr-práv-a-povinností/služby/",
 	publishDate: "2021-01-12",
 	editors: [{
 	  name: "Martin Nečaský",
-	  url: "https://martin.necasky.solid.community/profile/card#me",
+	  orcid: "0000-0002-5186-7734",
 	  company:    "Ministerstvo vnitra",
-	  companyURL: "http://www.mvcr.cz"
+	  companyURL: "https://www.mvcr.cz"
 	}],
 	inlineCSS: "true",
 	github: "https://github.com/opendata-mvcr/otevrene-formalni-normy/",
@@ -18,6 +17,10 @@ var respecConfig = {
 		data: [{
 			value: "Portál otevřených dat",
 			href: "https://data.gov.cz"
+		},{
+			value: "Kontaktní e-mail",
+			href: "mailto:otevrenadata@mvcr.cz"
 		}]
 	}],
+	thanks: "Tento dokument vznikl v rámci projektu OPZ č. CZ.03.4.74/0.0/0.0/15_025/0013983."
 };
